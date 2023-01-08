e3197d8f74a0603bb6e00e12410732a6518f0403
CodeTheRhythm <100514574+CodeTheRhythm@users.noreply.github.com>
2023-01-08 18:46:21 +0800

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+// const cors = require('cors');
+// const fileUpload = require('express-fileupload');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+
+const express = require('express');
+const app = express();
+
+// app.use(cors({exposedHeaders: ["login-token"]}));
+app.use(express.json());
+// app.use(fileUpload());
+app.use("/assets", express.static("common"));
+app.use("/images", express.static("images"));
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+mongoose.connect(process.env.CONNECTION_URL_OSE)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server started");
+});
